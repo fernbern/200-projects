fa011d2dc20513786ebb6ce3a785b43b38030881
fernbern <70728008+fernbern@users.noreply.github.com>
2021-09-23 17:41:48 -0700

    Add files via upload

---
+++ 46/main.js
@@ -0,0 +1,17 @@
+const div1 = document.querySelector('.box1');
+const div2 = document.querySelector('.box2');
+const div3 = document.querySelector('.box3');
+const btn = document.querySelector('.btn');
+
+div1.addEventListener('click', function(){
+    document.body.style.backgroundColor = '#0089f2';
+})
+div2.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'gray';
+})
+div3.addEventListener('click', function(){
+    document.body.style.backgroundColor = '#222';
+})
+btn.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'yellow';
+})
